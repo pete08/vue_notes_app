5446c039f565636ef853062f391acfd32f4dfa83
PeterH <pete08@gmail.com>
2020-08-16 12:33:57 -0500

    Add vue-multiselect Package to App.vue. Practice using VUE Packages with Vue app. Vue-Multiselect allows user to select from lists and many other things that would be cumbersome to build from scratch JS

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,33 @@
 <template>
-  <div id="app">
+  <div>
+    <label class="typo__label">Single select</label>
+    <multiselect v-model="value" :options="options" :searchable="false" :close-on-select="false" :show-labels="false" placeholder="Pick a value"></multiselect>
+    <pre class="language-json"><code>{{ value  }}</code></pre>
 
-    <Toolbar />
-    <note-container />
-    
   </div>
+  <!-- <multiselect v-model="value" :options="options"></multiselect> -->
+  
 </template>
 
 <script>
-import Toolbar from "./components/Toolbar.vue";
-import NoteContainer from "./components/NoteContainer.vue";
+import Multiselect from 'vue-multiselect';
+
+// register globally
+// Vue.component('multiselect', Multiselect);
 
 export default {
-  name: 'app',
-  components: {
-    Toolbar,  //Components should ALWAYS be PascalCase
-    NoteContainer, //Components should ALWAYS be PascalCase  
-  }
+  // OR register locally
+  components: { 
+    Multiselect
+  },
 
+  data () {
+    return {
+      value: '',
+      options: ['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched']
+    };
+  }
 };
 </script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
-<style>
-/* RESET */
-* {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  outline: none;
-  box-sizing: border-box;
-}
-/* LAYOUT */
-#app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-.toolbar {
-  padding: 0.5em;
-}
-.toolbar-button, .toolbar-search {
-  padding: inherit;
-  border-radius: 0.3em;
-}
-.toolbar-search {
-  float: right;
-}
-.note-container {
-  display: flex;
-  flex: 1;
-}
-.note-selectors {
-  flex: 0 0 13em;
-}
-.note-selector {
-  padding: 1em;
-}
-.note-selector p {
-  margin: 0;
-}
-.note-editor {
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-}
-.note-editor-info {
-  padding: 0.5em;
-  text-align: center;
-}
-.note-editor-input {
-  display: flex;
-  flex: 1;
-  width: 100%;
-  padding: 0 2em 0 2em;
-}
-/* COLORS */
-* {
-  color: #454545;
-  background-color: #FAFAF8;
-}
-.toolbar {
-  background-color: #DCDADC;
-}
-.toolbar-button {
-  background-color: #FFFFFF;
-}
-.toolbar-button:active {
-  background-color: #AAAAAA;
-}
-.note-selectors {
-  border-right: 1px solid #DCDADC;
-}
-.note-selector {
-  border-bottom: 1px solid #DCDADC;
-}
-.note-selector.active {
-  background-color: #FCE18D;
-}
-.note-selector-title {
-  background-color: inherit;
-}
-.note-selector-timestamp {
-  color: #626262;
-  background-color: inherit;
-}
-.note-editor-info {
-  color: #DCDADC;
-}
-/* TYPOGRAPHY */
-body {
-  font-family: sans-serif;
-}
-.note-selector-title {
-  font-weight: bold;
-}
-.note-selector-timestamp {
-  font-size: 0.7em;
-}
-.note-editor, .note-editor-input {
-  font-size: 0.9em;
-}
-</style>
