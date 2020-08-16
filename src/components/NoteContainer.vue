a214c48ba19a39c37641d5f4d54dde42876e02be
PeterH <pete08@gmail.com>
2020-08-16 10:53:03 -0500

    Add submodule container files to the NoteContainer file including: NoteEditor.vue and NoteSelectors.vue

--- src/components/NoteContainer.vue
+++ src/components/NoteContainer.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="note-container">
-    <div class="note-selectors">
-      <div class="note-selector active">
-        <p class="note-selector-title">First note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-      <div class="note-selector">
-        <p class="note-selector-title">Second note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-      <div class="note-selector">
-        <p class="note-selector-title">Third note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-    </div>
-    <div class="note-editor">
-      <p class="note-editor-info">Timestamp here...</p>
-      <textarea class="note-editor-input">
-        First note...
-        
-        Note text here...
-      </textarea>
-    </div>
+    <note-selectors />
+    <note-editor />
   </div>
 </template>
 
 <script>
+//DONT FORGET: "." to establish the directory location within app
+import NoteSelectors from "./NoteSelectors.vue";
+import NoteEditor from "./NoteEditor.vue";
+
 export default {
   name: "note-container",
+  components: {
+    NoteSelectors,
+    NoteEditor,
+
+  }
 };
 </script>
